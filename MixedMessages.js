e2d0639fa7613100c440f5569a4aba67fbc46556
LAPTOP-UT2HKRSM\Abbas <ashafi1430@gmail.com>
2021-05-23 21:13:46 +0100

    Mixed Message compelet.

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,2 +1,10 @@
-console.log("Test!");
-console.log("Testing123!");
+//Joke of the day generator
+// - run program to output a random joke from a static list of jokes.
+
+let jokes = ["Joke 0", "Joke 1","Joke 2","Joke 3", "Joke 4", "Joke 5", "Joke 6", "Joke 7", "Joke 8", "Joke 9", "Joke 10", "Joke 11", "Joke 12"];
+
+let randomNumber = Math.floor(Math.random() * jokes.length);
+
+console.log(`Print a random joke: ${jokes[randomNumber]}`);
+
+
